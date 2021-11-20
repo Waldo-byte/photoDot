c09fddf11235cef339b8ef8c731aa6060be65810
Waldo-byte <69579496+Waldo-byte@users.noreply.github.com>
2021-11-20 21:11:32 +0200

    File Write

--- photoDotWeb/src/main/java/photoDot/test/web/sb/controller/FileWriteController.java
+++ photoDotWeb/src/main/java/photoDot/test/web/sb/controller/FileWriteController.java
@@ -1,12 +1,15 @@
 package photoDot.test.web.sb.controller;
 
-import com.google.api.gax.paging.Page;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.ReadChannel;
-import com.google.cloud.storage.*;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceAsyncClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobItem;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoProperties;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -18,31 +21,33 @@ import java.util.List;
 @RequestMapping("/file")
 @CrossOrigin("*")
 public class FileWriteController {
-    StorageOptions storageOptions;
 
-    {
-        try {
-            storageOptions = StorageOptions.newBuilder()
-                    .setProjectId("natural-engine-332701")
-                    .setCredentials(GoogleCredentials.fromStream(new
-                            FileInputStream("src/main/resources/gcp-credentials.json"))).build();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+   // @Value("${azure.myblob.url}")
+    private String azureurl = "BlobEndpoint=https://retryspringboot.blob.core.windows.net/;QueueEndpoint=https://retryspringboot.queue.core.windows.net/;FileEndpoint=https://retryspringboot.file.core.windows.net/;TableEndpoint=https://retryspringboot.table.core.windows.net/;SharedAccessSignature=sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2021-12-09T01:59:16Z&st=2021-11-20T17:59:16Z&spr=https,http&sig=OoHviDFFCRcu83L0ttlwSuwXKu16vLP0RlqtnQ7I2SY%3D";
+
+   // @Value("${azure.myblob.container}")
+    private String containerName = "waldocontainer";
+
+    private BlobContainerClient containerClient(){
+        BlobServiceClient serviceClient = new BlobServiceClientBuilder()
+                .connectionString(azureurl).buildClient();
+        BlobContainerClient container = serviceClient.createBlobContainer(containerName);
+        return container;
     }
 
-    private Storage storage = storageOptions.getService();
-    private String bucketName = "springbootapp";
-    private Bucket bucket = storage.get("springbootapp");
-
-    @GetMapping("/getall")
-    public List<String> readFromFile() throws Exception {
-        List<String> blobs = new ArrayList<>();
-        for(Blob currentBlob: bucket.list().iterateAll(){
-            Page<Blob>(currentBlob);
+    @GetMapping("/all")
+    public String listFiles(){
+        BlobContainerClient containerClient = containerClient();
+        List<String> listfiles = new ArrayList<>();
+        String item = null;
+        for(BlobItem blobItem: containerClient.listBlobs()){
+            listfiles.add(blobItem.getName());
+            item = blobItem.getName();
         }
-        return next;
+        return item;
     }
+
+
 
 
 
